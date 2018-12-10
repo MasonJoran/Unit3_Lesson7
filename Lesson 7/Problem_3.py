d3c9d19618595366860b490cb823684f7e78936d
MasonJoran <43784850+MasonJoran@users.noreply.github.com>
2018-12-10 11:27:55 -0500

    Add files via upload

---
+++ Lesson 7/Problem_3.py
@@ -0,0 +1,24 @@
+print('-' * 60)
+
+print('Hello! I will be predicting in which year you will turn 100 years old! ')
+print()
+
+age = input('What is your age today?: ')
+age = int(age)
+
+year = input('What is the current year?: ')
+year = int(year)
+
+time = 100 - age
+
+
+print()
+
+print("...")
+print("...")
+print("...")
+
+print()
+
+print('You will be 100 years old in about... ' + str(time) + ' years! Congratulations!')
+print('-' * 60)
---
+++ Lesson 7/Problem_4.py
@@ -0,0 +1,26 @@
+print('-' * 60)
+
+print()
+
+print('Hello, I am Morbid-bot. I will be determining when you will die in the future. ')
+
+age = input('Please enter your current age: ')
+age = int(age)
+
+year = input('Lastly, enter the current year: ')
+year = int(year)
+
+print()
+
+print(' ...Processing... ')
+print(' ...Processing... ')
+print(' ...Processing... ')
+
+print()
+
+expiration = 79 - age
+expiration = year + expiration
+
+expiration = int(expiration)
+
+print('You will expire on the year... ' + str(expiration) + '. How sad.'
